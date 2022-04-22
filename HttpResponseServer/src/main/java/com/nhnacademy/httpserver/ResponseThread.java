cc4c0223e809798cc0c8c05af5a00b1d0071604a
ComSun <computerhermit96@gmail.com>
2022-04-22 14:34:49 +0900

    쿼리파라미터 구현

--- HttpResponseServer/src/main/java/com/nhnacademy/httpserver/ResponseThread.java
+++ HttpResponseServer/src/main/java/com/nhnacademy/httpserver/ResponseThread.java
@@ -6,20 +6,15 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import org.json.simple.JSONObject;
 
 public class ResponseThread implements Runnable {
     Socket socket;
@@ -49,23 +44,24 @@ public class ResponseThread implements Runnable {
 
             String[] temp = message.split("\n");
             for (int i = 0; i < temp.length; i++) {
-                if(temp[i].equals("\r")) {
+                if (temp[i].equals("\r")) {
                     isChecked = true; //requestHeader body 구분
                 }
-                if(!isChecked){
+                if (!isChecked) {
                     packetSave.add(temp[i]);
-                }else{
-                    jsonStr+=temp[i]; //requestBody
+                } else {
+                    jsonStr += temp[i]; //requestBody
                 }
             }
             System.out.println(jsonStr);
             System.out.println("여기까진 OK!");
             System.out.println(socket.getLocalAddress());
-            System.out.println("json Str = "+ jsonStr.trim()); // str 앞에 \r이 붙어있어서 씹힌다. 그러므로 trim으로 없애 넘겨줌.
+            System.out.println(
+                "json Str = " + jsonStr.trim()); // str 앞에 \r이 붙어있어서 씹힌다. 그러므로 trim으로 없애 넘겨줌.
 
             //-----------------Requset 패킷 받아온 값들 .
 
-            ClassPacket request = new ClassPacket(packetSave,jsonStr.trim());
+            ClassPacket request = new ClassPacket(packetSave, jsonStr.trim());
 
             PrintStream printStream = new PrintStream(socket.getOutputStream());
 
@@ -73,32 +69,32 @@ public class ResponseThread implements Runnable {
             Map<String, Object> map = new HashMap<>();
             OffsetDateTime currentTime = OffsetDateTime.now();
 
-            if(request.getUrlPath().contains("/ip")){
+            if (request.getUrlPath().contains("/ip")) {
                 String json;
-                map.put("origin", socket.getInetAddress().toString().replace("/",""));
+                map.put("origin", socket.getInetAddress().toString().replace("/", ""));
                 json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
                 System.out.println(json);   // pretty-print
                 printResponseHeader(printStream, currentTime, json);
                 printStream.println(json);
                 printStream.flush();
 
-            }else if(request.getUrlPath().contains("/get")){
+            } else if (request.getUrlPath().contains("/get")) {
                 Map<String, String> headerMap = new HashMap<>();
                 headerMap.put("Accept", request.getRequestHeader("Accept"));
                 headerMap.put("Host", request.getRequestHeader("Host"));
                 headerMap.put("User-Agent", request.getRequestHeader("User-Agent"));
-                map.put("args", request.getUrlPathArgs());
+                map.put("args", createArgsMap(request.getUrlPathArgs()));
                 map.put("hearders", headerMap);
-                map.put("origin", socket.getInetAddress().toString().replace("/",""));
-                map.put("url",socket.getLocalAddress().toString().replace("/","")+"/get");
-                String responseJsonBody = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
-                if(!request.getUrlPath().contains("?")){
-                    printResponseHeader(printStream, currentTime, responseJsonBody);
-                }
-                printStream.println(responseJsonBody);
-            }else if(request.getUrlPath().contains("/post")){
+                map.put("origin", socket.getInetAddress().toString().replace("/", ""));
+                map.put("url", socket.getLocalAddress().toString().replace("/", "") + "/get");
+                String responseJsonBody =
+                    mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
 
-            }else{
+                printResponseHeader(printStream, currentTime, responseJsonBody);
+                printStream.println(responseJsonBody);
+            } else if (request.getUrlPath().contains("/post")) {
+
+            } else {
                 // output으로 4xx에러 발생 시키기.
             }
 
@@ -107,11 +103,13 @@ public class ResponseThread implements Runnable {
         }
     }
 
-    private void printResponseHeader(PrintStream printStream, OffsetDateTime currentTime, String json) {
+    private void printResponseHeader(PrintStream printStream, OffsetDateTime currentTime,
+                                     String json) {
         printStream.println("HTTP/1.1 200 OK");
-        printStream.println("Date: " + currentTime.format(DateTimeFormatter.RFC_1123_DATE_TIME)); //TODO 타임 포맷팅 UTF8?
+        printStream.println("Date: " +
+            currentTime.format(DateTimeFormatter.RFC_1123_DATE_TIME)); //TODO 타임 포맷팅 UTF8?
         printStream.println("Content-Type: application/json");
-        printStream.println("Content-Length: "+ json.length());
+        printStream.println("Content-Length: " + json.length());
         printStream.println("Connection: keep-alive");
         printStream.println("Server: gunicorn/19.9.0");
         printStream.println("Access-Control-Allow-Origin: *");
@@ -119,17 +117,28 @@ public class ResponseThread implements Runnable {
         printStream.println();
     }
 
-    public void ipController(ClassPacket request){
+    public void ipController(ClassPacket request) {
 
     }
-    public void getController(ClassPacket request){
+
+    public void getController(ClassPacket request) {
 
     }
-    public void postController(ClassPacket request){
+
+    public void postController(ClassPacket request) {
 
     }
-    public void responseHeader(ClassPacket request){
 
+    public void responseHeader(ClassPacket request) {
+
+    }
+
+    public Map<String, String> createArgsMap(String input) {
+
+        String[] args = input.split("=");
+        Map<String, String> returnMap = new HashMap<>();
+        returnMap.put(args[0], args[1]);
+        return returnMap;
     }
 
 
